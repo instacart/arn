d535e190275bef9846e7b7469f062538aac953be
Francois Campbell <campbell.francois@gmail.com>
2020-08-04 15:57:48 -0400

    move STS into IAM

--- src/arn/iam.py
+++ src/arn/iam.py
@@ -13,3 +13,15 @@ class RoleArn(Arn):
     REST_PATTERN = re.compile(r"role/(?P<name>.*)")
 
     name: str = ""
+
+
+@dataclass
+class AssumedRoleArn(Arn):
+    """ARN for an `IAM Assumed Role <https://docs.aws.amazon.com/IAM/latest/UserGuide/id_credentials_temp_use-resources.html>`_."""
+
+    REST_PATTERN = re.compile(
+        r"assumed-role/(?P<role_name>.*)/(?P<role_session_name>.*)"
+    )
+
+    role_name: str = ""
+    role_session_name: str = ""
