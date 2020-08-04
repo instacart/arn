300bccb4e82892c37575c9ae99167bd18eb5ca0f
Francois Campbell <campbell.francois@gmail.com>
2020-08-04 16:02:17 -0400

    add s3

--- src/arn/s3.py
+++ src/arn/s3.py
@@ -1,21 +1,29 @@
+"""ARNs for `AWS S3 <https://docs.aws.amazon.com/IAM/latest/UserGuide/list_amazons3.html#amazons3-resources-for-iam-policies>`_."""
+
 import re
 
 from arn import Arn
 
 
 class AccessPointArn(Arn):
+    """ARN for an `S3 Access Point <https://docs.aws.amazon.com/AmazonS3/latest/dev/access-points.html>`_."""
+
     REST_PATTERN = re.compile(r"accesspoint/(?P<name>.*)")
 
     name: str = ""
 
 
 class BucketArn(Arn):
+    """ARN for an `S3 Bucket <https://docs.aws.amazon.com/AmazonS3/latest/dev/UsingBucket.html>`_."""
+
     REST_PATTERN = re.compile(r"(?P<name>.*)")
 
     name: str = ""
 
 
 class ObjectArn(Arn):
+    """ARN for an `S3 Object <https://docs.aws.amazon.com/AmazonS3/latest/dev/UsingObjects.html>`_."""
+
     REST_PATTERN = re.compile(r"(?P<bucket_name>.*)/(?P<object_name>.*)")
 
     bucket_name: str = ""
@@ -23,6 +31,8 @@ class ObjectArn(Arn):
 
 
 class JobArn(Arn):
+    """ARN for an `S3 Batch Job <https://docs.aws.amazon.com/AmazonS3/latest/dev/batch-ops-managing-jobs.html>`_."""
+
     REST_PATTERN = re.compile(r"job/(?P<id>.*)")
 
     id: str = ""
