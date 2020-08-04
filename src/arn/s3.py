cbe8321884e14e5dd1003d69eaae83d28137832f
Francois Campbell <campbell.francois@gmail.com>
2020-08-04 10:50:11 -0400

    add s3 arns

---
+++ src/arn/s3.py
@@ -0,0 +1,28 @@
+import re
+
+from arn import Arn
+
+
+class AccessPointArn(Arn):
+    REST_PATTERN = re.compile(r"accesspoint/(?P<name>.*)")
+
+    name: str = ""
+
+
+class BucketArn(Arn):
+    REST_PATTERN = re.compile(r"(?P<name>.*)")
+
+    name: str = ""
+
+
+class ObjectArn(Arn):
+    REST_PATTERN = re.compile(r"(?P<bucket_name>.*)/(?P<name>.*)")
+
+    bucket_name: str = ""
+    name: str = ""
+
+
+class JobArn(Arn):
+    REST_PATTERN = re.compile(r"job/(?P<id>.*)")
+
+    id: str = ""
---
+++ tests/test_arn_s3.py
@@ -0,0 +1,26 @@
+from arn import s3
+
+
+def test_parses_access_point_arn(make_arn):
+    arn = make_arn("s3", "accesspoint/foo")
+    result = s3.AccessPointArn(arn)
+    assert result.name == "foo"
+
+
+def test_parses_bucket_arn(make_arn):
+    arn = make_arn("s3", "foo")
+    result = s3.BucketArn(arn)
+    assert result.name == "foo"
+
+
+def test_parses_object_arn(make_arn):
+    arn = make_arn("s3", "foo/bar")
+    result = s3.ObjectArn(arn)
+    assert result.bucket_name == "foo"
+    assert result.name == "bar"
+
+
+def test_parses_job_arn(make_arn):
+    arn = make_arn("s3", "job/foo")
+    result = s3.JobArn(arn)
+    assert result.id == "foo"
