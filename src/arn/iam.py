79684b648dbd31698411285ac3a6d4bc61dafc3e
Francois Campbell <campbell.francois@gmail.com>
2020-08-04 15:53:15 -0400

    add iam

--- src/arn/iam.py
+++ src/arn/iam.py
@@ -1,3 +1,5 @@
+"""ARNs for `AWS IAM <https://docs.aws.amazon.com/IAM/latest/UserGuide/list_identityandaccessmanagement.html#identityandaccessmanagement-resources-for-iam-policiess>`_."""
+
 import re
 from dataclasses import dataclass
 
@@ -6,6 +8,8 @@ from . import Arn
 
 @dataclass
 class RoleArn(Arn):
+    """ARN for an `IAM Role <https://docs.aws.amazon.com/IAM/latest/UserGuide/id_roles.html>`_."""
+
     REST_PATTERN = re.compile(r"role/(?P<name>.*)")
 
     name: str = ""
