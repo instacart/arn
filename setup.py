d558b54ef682a95bd95a98985c622f2150556653
Francois Campbell <francoiscampbell@users.noreply.github.com>
2020-08-20 12:49:11 -0400

    Add the readme to the PyPI description (#4)

--- setup.py
+++ setup.py
@@ -1,7 +1,18 @@
+# read the contents of your README file
+# taken from https://packaging.python.org/guides/making-a-pypi-friendly-readme/
+from os import path
+
 from setuptools import find_packages, setup
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md")) as f:
+    long_description = f.read()
 
 setup(
     name="arn",
+    description="A Python library for parsing AWS ARNs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     python_requires=">=3.6",
@@ -19,5 +30,5 @@ setup(
             "wheel==0.34.2",
         ]
     },
-    version="0.1.0",
+    version="0.1.2",
 )
