41ba2a6ba8a0cb2c51f1c83a8f64acc5592d0958
Alexandre Sieira <alexandre.sieira@gmail.com>
2021-03-16 17:34:29 -0300

    Update setup.py
    Add PyPI classifiers and links to source code and documentation.

--- setup.py
+++ setup.py
@@ -11,8 +11,22 @@ version = (this_directory / "version.txt").read_text().strip()
 setup(
     name="arn",
     description="A Python library for parsing AWS ARNs",
+    url="https://github.com/instacart/arn",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    project_urls={
+        "Documentation": "https://arn.readthedocs.io/en/latest/",
+        "Source Code": "https://github.com/instacart/arn"
+    },
+    license="BSD 3-Clause Clear License",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     python_requires=">=3.6",
